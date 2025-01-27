640a45c3846a47820e257a857665d886160e63fb
Dagmawi <122219411+DAGMAWI-AT@users.noreply.github.com>
2025-01-27 08:20:34 -0800

    Update db.js

--- backend/configuration/db.js
+++ backend/configuration/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // MongoDB URI
-const uri = "mongodb://${{proccess.env.MONGO_INITDB_ROOT_USERNAME}}:${{proccess.env.MONGO_INITDB_ROOT_PASSWORD}}@${{proccess.env.MONGO_PUBLIC_URL}}:${{proccess.env.RAILWAY_TCP_PROXY_PORT}}";
+const uri = "mongodb://${{process.env.MONGO_INITDB_ROOT_USERNAME}}:${{process.env.MONGO_INITDB_ROOT_PASSWORD}}@${{process.env.MONGO_PUBLIC_URL}}:${{process.env.RAILWAY_TCP_PROXY_PORT}}";
   //"mongodb+srv://finance:finance_offices@cluster0.ixomm.mongodb.net/?retryWrites=true&w=majority";
 
 // Create MongoClient instance
