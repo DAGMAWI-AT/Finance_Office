12e05bf58cc573e076960b5403580d546d8a14a3
Dagmawi <122219411+DAGMAWI-AT@users.noreply.github.com>
2025-01-27 08:27:41 -0800

    Update db.js

--- backend/configuration/db.js
+++ backend/configuration/db.js
@@ -1,26 +1,14 @@
-// src/config/db.js
-require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config();
+const { MongoClient } = require("mongodb");
 
-// MongoDB URI
-const uri = "mongodb://${{process.env.MONGO_INITDB_ROOT_USERNAME}}:${{process.env.MONGO_INITDB_ROOT_PASSWORD}}@${{process.env.MONGO_PUBLIC_URL}}:${{process.env.RAILWAY_TCP_PROXY_PORT}}";
-  //"mongodb+srv://finance:finance_offices@cluster0.ixomm.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URL;
+console.log("MongoDB URI:", uri);
 
-// Create MongoClient instance
-const client = new MongoClient(uri, {
-  serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true },
-});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Connect to MongoDB
-async function connectDB() {
-  try {
-    await client.connect();
-    console.log("MongoDB connected successfully");
-  } catch (err) {
-    console.error("MongoDB connection failed:", err);
-    process.exit(1); // Exit the process if the connection fails
-  }
-}
+client.connect()
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("Failed to connect to MongoDB:", err));
 
 // Export the client and connect function
 module.exports = { client, connectDB };
