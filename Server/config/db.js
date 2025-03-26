652c0a77ae4cb33a30a9ab23acd042519fa9c220
DAGMAWI-AT <vvman1717@gmail.com>
2025-03-26 13:09:40 -0700

    commit

--- Server/config/db.js
+++ Server/config/db.js
@@ -129,16 +129,16 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "finance_office",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   // queueLimit: 0,
-  // connectTimeout: 10000, // 10 seconds timeout
+  connectTimeout: 10000, // 10 seconds timeout
   waitForConnections: true,
-  connectTimeout: 60000, // Increase to 60 seconds
+  // connectTimeout: 60000, // Increase to 60 seconds
   acquireTimeout: 60000, // Increase to 60 seconds
   queueLimit: 0
 
